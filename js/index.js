ababda5ca58c988061519c94fdcbd25faec7e2f5
Kim Sori <sori.designer@renosoft.kr>
2019-08-05 17:07:03 +0900

    20190805

--- js/index.js
+++ js/index.js
@@ -1,3 +1,45 @@
+// Hamburger Button and Slide Menu
+$('.menu').click(function() {
+	$('.aside').toggleClass('hide');
+	//$(this).toggleClass('open');
+})
+
+
+
+// Accordion Menu
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		// more then one submenu open?
+		this.multiple = multiple || false;
+
+		var dropdownlink = this.el.find('.dropdownlink');
+		dropdownlink.on('click', {
+			el : this.el,
+			multiple : this.multiple
+		}, this.dropdown);
+	};
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el, $this = $(this),
+		// this is the ul.submenuItems
+		$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			// show only one menu at the same time
+			$el.find('.submenuItems').not($next).slideUp().parent()
+					.removeClass('open');
+		}
+	}
+
+	var accordion = new Accordion($('.accordion-menu'), false);
+})
+
+
+
 // Tab Menu
 $(document).ready(function() {
 	$('.tab-menu li').click(function() {
@@ -12,35 +54,51 @@ $(document).ready(function() {
 })
 
 
-// Selectbox
-$(document).ready(
-    function() {
-        $('.selectbox').each(
-                function() {
 
-                    var $this = $(this), firstVal = $this.find(
-                            ".selectbox-options span:eq(0)").text();
 
-                    // Set first option
-                    $this.find(".selectbox-current .selected").text(
-                            firstVal);
+$(document).ready(function() {
+	$('.input-container input, .search .selectbox, .search input').click(function() {
 
-                    // Show/hide drop down
-                    $this.find(".selectbox-current").bind("click",
-                            function() {
-                                if (!$this.find(".options").is(":visible")) {
-                                    $this.find(".selectbox-options").slideDown(150);
-                                } else {
-                                    $this.find(".selectbox-options").slideUp(150);
-                                }
-                            });
+		$('.input-container, .search').removeClass('active');
+        $(this).parent().addClass('active')
+    })
+})
 
-                    // Set selected value
-                    $this.find(".selectbox-options span").bind("click",
-                        function() {
-                            val = $(this).text();
-                            $this.find(".selected").text(val);
-                            $this.find(".selectbox-options").slideUp(150);
-                    });
-        });
+
+// Modal
+function modal(type) {
+	$(".mask").addClass("active");
+	if (type == "modal1") {
+		$(".modal-01").show();
+	} else if(type == "modal2") {
+		$(".modal-02").show();
+	}
+}
+
+function closeModal() {
+	$(".mask").removeClass("active");
+	$(".modal").hide();
+}
+
+$(".close, .cancel, .mask").on("click", function() {
+	closeModal();
 });
+$(document).keyup(function(e) {
+	if (e.keyCode == 27) {
+		closeModal();
+	}
+});
+
+
+
+// Tooltip
+$(document).ready(function() {
+    $(".tooltip").click(function () {
+    var $title = $(this).find(".title");
+    if (!$title.length) {
+      $(this).append('<span class="title">' + $(this).attr("title") + '</span>');
+    } else {
+      $title.remove();
+    }
+  })
+})
