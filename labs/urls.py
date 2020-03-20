3c1b8df1aa769e9cc11761486e25c39d402cf607
coralcat <sori.coralcat@gmail.com>
2020-03-20 15:54:19 +0900

    요소 추가 페이지 생성

--- labs/urls.py
+++ labs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LabsListView,LabsCreateView
+from .views import LabsListView, LabsCreateView
 
 urlpatterns = [
     path('', LabsListView.as_view(), name='list'),
