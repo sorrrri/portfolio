4af832c60d87ad541a3f2e52d805e162c0c2024f
coralcat <sori.coralcat@gmail.com>
2020-03-20 13:13:10 +0900

    __str__ 메서드 추가

--- labs/models.py
+++ labs/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Labs(models.Model):
     site_name = models.CharField(max_length=100)
     url = models.URLField('Site URL')
+
+    def __str__(self):
+        # 객체를 출력할 때 나타날 값
+        return "이름: " + self.site_name + ", 주소: " + self.url
