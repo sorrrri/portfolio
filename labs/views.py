4663529f7bf122b6b97b9135a9ce7ca331aee677
coralcat <sori.coralcat@gmail.com>
2020-03-20 14:04:50 +0900

    목록 요소 추가 기능 구현

--- labs/views.py
+++ labs/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Labs
 
 class LabsListView(ListView):
     model = Labs
+
+
+
+class LabsCreateView(CreateView):
+    model = Labs
+    fields = ['site_name', 'url']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'
