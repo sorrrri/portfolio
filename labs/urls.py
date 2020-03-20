bf62062817f9f50b3249ac710fa993cfb9cb95df
coralcat <sori.coralcat@gmail.com>
2020-03-20 14:07:59 +0900

    urlpatterns 변수에 create path 추가

--- labs/urls.py
+++ labs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import LabsListView
+from .views import LabsListView,LabsCreateView
 
 urlpatterns = [
     path('', LabsListView.as_view(), name='list'),
+    path('create/', LabsCreateView.as_view(), name='create')
 ]
