26cc74d5cfc8724e55c404ab80607de6c820d96c
coralcat <sori.coralcat@gmail.com>
2020-03-20 16:35:21 +0900

    삭제 페이지 구현

--- labs/views.py
+++ labs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 
@@ -9,22 +9,20 @@ from .models import Labs
 class LabsListView(ListView):
     model = Labs
 
-
-
 class LabsCreateView(CreateView):
     model = Labs
     fields = ['site_name', 'url']
     success_url = reverse_lazy('list')
     template_name_suffix = '_create'
 
-
-
 class LabsDetailView(DetailView):
     model = Labs
-
-
 
 class LabsUpdateView(UpdateView):
     model = Labs
     fields = ['site_name', 'url']
     template_name_suffix = '_update'
+
+class LabsDeleteView(DeleteView):
+    model = Labs
+    success_url = reverse_lazy('list')
