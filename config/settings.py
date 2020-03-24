2eba4c8516d3030834b3e2094ca3a43b12b815a1
coralcat <sori.coralcat@gmail.com>
2020-03-24 10:21:02 +0900

    배포환경 세팅

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q8#lrdn25$gvfgz40ogpz9x!zs=rzqwukne&76^5=0!o$e%6(3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
