aa7db39c888b7db30782a5709cafb9c0cddb46fb
coralcat <sori.coralcat@gmail.com>
2020-03-30 11:35:26 +0900

    collectstatic

---
+++ static_files/js/index.js
@@ -0,0 +1,59 @@
+function modal() {
+    $(".modal, .backdrop").addClass("active");
+}
+
+function closeModal() {
+    $(".modal, .backdrop").removeClass("active");
+}
+
+$(".close, .modal .cancel, .backdrop").on("click", function () {
+    closeModal();
+});
+
+/*
+$(document).keyUp(function(e) {
+    if(e.keyCode === 27) {
+        closeModal();
+    }
+});*/
+
+
+function previewFile() {
+
+    var preview = document.querySelector('img');
+    var file = document.querySelector('#id_photo').files[0];
+    var reader = new FileReader();
+    var label = document.querySelector('label');
+    var icon = document.querySelector('ion-icon');
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    }
+
+    label.classList.add('active');
+    icon.parentNode.removeChild(icon);
+};
+
+function selectbox() {
+   var selectboxCurrent = document.getElementsByClassName('selectbox-current');
+
+}
+
+$(document).ready(function () {
+
+    // Selectbox
+    $(".selectbox-current").click(function () {
+        $(".selectbox-options").addClass("active");
+    });
+
+    $(".selectbox-options li").click(function () {
+        $(".selectbox-current .selected").text($(this).text());
+        $(".selectbox-options").removeClass("active");
+    });
+});
