31e726b692afa040c70cca9b595c4f9faf5ce687
coralcat <sori.coralcat@gmail.com>
2020-04-13 15:14:04 +0900

    Dark Theme 추가

--- src/index.js
+++ src/index.js
@@ -5,3 +5,25 @@ window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 })
+
+
+
+// Disabling and enabling buttons
+// $('.search input').on('keyup', function () {
+//     if ($('button').attr('disabled','disabled')) {
+//         $('button').removeAttr('disabled');
+//     }
+// });
+
+
+// Toggle Button
+$('.cb-value').click(function() {
+    var mainParent = $(this).parent('.btn-toggle');
+    if($(mainParent).find('input.cb-value').is(':checked')) {
+        $(mainParent).addClass('dark');
+        $(".container").addClass("theme-dark");
+    } else {
+        $(mainParent).removeClass('dark');
+        $(".container").removeClass("theme-dark");
+    }
+})
