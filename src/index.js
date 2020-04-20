cd3cc09425d7a01bc73de3193379b26aed676533
coralcat <sori.coralcat@gmail.com>
2020-04-20 13:23:56 +0900

    모달 js

--- src/index.js
+++ src/index.js
@@ -26,6 +26,22 @@ $(".navigation a").mouseup(function () {
 });
 
 
+
+// Modal
+$(".btn-COx-MAP").on("click", function () {
+    $(".modal-COx-MAP, .modal-backdrop").addClass("active")
+})
+
+$(".btn-ETCO2").on("click", function () {
+    $(".modal-ETCO2, .modal-backdrop").addClass("active")
+})
+
+$(".modal-backdrop").on("click", function() {
+    $(".modal-backdrop, .modal").removeClass("active");
+});
+
+
+
 // Toggle Button
 /*$('.cb-value').click(function () {
     var mainParent = $(this).parent('.btn-toggle');
