a4673ec56ca645a20ba9d66fa32334885546cda9
coralcat <sori.coralcat@gmail.com>
2020-06-15 16:32:41 +0900

    static 설정중

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +8,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('operationalDefinition.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
