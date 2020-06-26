f8ebecb092aa736a548434c925ba3c89765bf987
coralcat <sori.coralcat@gmail.com>
2020-06-26 10:40:44 +0900

    관리자화면 진입모드 설정

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,30 @@
-$("#login-button").click(function(event){
-    event.preventDefault();
+window.onload = () => {
+    /* =====================================================
+       Login Page - Temporary Scripts
+       ===================================================== */
+    const loginButton = document.querySelector('#login-button')
+    const loginForm = document.querySelector('.login-form')
+    const wrapper = document.querySelector('.wrapper')
 
-    $('form').fadeOut(500);
-    $('.wrapper').addClass('form-success');
-});
+/*    loginButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        loginForm.fadeOut(500);
+        wrapper.classList.add('form-success')
+    })*/
+
+
+
+    /* =====================================================
+       Header - Toggle Button
+       ===================================================== */
+    const toggleAdmin = document.querySelector('.toggle-admin')
+    const toggleCheckbox = document.querySelector('.toggle-checkbox')
+
+    toggleAdmin.addEventListener('click', () => {
+        if(toggleCheckbox.checked) {
+            location.href='./admin/user_list.html'
+        } else {
+            location.href='../index.html'
+        }
+    })
+}
