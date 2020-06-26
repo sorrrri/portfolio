dbf63fdd80c999929a9d997463271855aa9fc96c
coralcat <sori.coralcat@gmail.com>
2020-06-26 17:24:28 +0900

    관리자 정보 수정 페이지 화면 작업

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,9 @@
-window.onload = () => {
-    /* =====================================================
-       Login Page - Temporary Scripts
-       ===================================================== */
-    const loginButton = document.querySelector('#login-button')
-    const loginForm = document.querySelector('.login-form')
-    const wrapper = document.querySelector('.wrapper')
+/* =====================================================
+   Login Page - Temporary Scripts
+   ===================================================== */
+const loginButton = document.querySelector('#login-button')
+const loginForm = document.querySelector('.login-form')
+const wrapper = document.querySelector('.wrapper')
 
 /*    loginButton.addEventListener('click', (e) => {
         e.preventDefault()
@@ -13,18 +12,37 @@ window.onload = () => {
     })*/
 
 
+/* =====================================================
+   Header - Toggle Button
+   ===================================================== */
+const toggleAdmin = document.querySelector('.toggle-admin')
+const toggleCheckbox = document.querySelector('.toggle-checkbox')
 
-    /* =====================================================
-       Header - Toggle Button
-       ===================================================== */
-    const toggleAdmin = document.querySelector('.toggle-admin')
-    const toggleCheckbox = document.querySelector('.toggle-checkbox')
+toggleAdmin.addEventListener('click', () => {
+    if (toggleCheckbox.checked) {
+        location.href = './admin/user_list.html'
+    } else {
+        location.href = '../index.html'
+    }
+})
 
-    toggleAdmin.addEventListener('click', () => {
-        if(toggleCheckbox.checked) {
-            location.href='./admin/user_list.html'
-        } else {
-            location.href='../index.html'
-        }
-    })
+
+
+
+
+/*
+const addInput = document.querySelectorAll('.add-input')
+addInputFunction = () => {
+    const createInputContainer = `
+                <div class="input-container" onclick="location.href='#modal-set-event'">
+                    <input type="text" >
+                </div>`
+    addInput.appendChild(createInputContainer)
 }
+
+
+function addDrugs() {
+    var drugs = document.getElementById("drugs");
+    var clone = drugs.firstElementChild.cloneNode(true);
+    drugs.appendChild(clone);
+}*/
