27e06e969c36f0cb26d4e9eca091694edcad499a
coralcat <sori.coralcat@gmail.com>
2020-07-03 17:30:57 +0900

    trigger 효과 내기

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+/*
 const body = document.body,
     scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
     height = scrollWrap.getBoundingClientRect().height - 1,
@@ -17,8 +18,25 @@ function smoothScroll() {
 }
 
 smoothScroll();
+*/
+
+
 
 $(function () {
+    var fadeIn = $('section > *').toArray();
+    $(window).scroll(function () {
+        fadeIn.forEach(function (item) {
+            if ($(this).scrollTop() >= $(item).offset().top - 400) {
+                $(item).css('opacity', '1')
+            }
+        });
+    });
+    fadeIn.forEach(function (item) {
+        if ($(this).scrollTop() >= $(item).offset().top - 400) {
+            $(item).css('opacity', '1')
+        }
+    });
+
     var elements = $(".text, .image").toArray();
     $(window).scroll(function () {
         elements.forEach(function (item) {
@@ -30,6 +48,13 @@ $(function () {
     elements.forEach(function (item) {
         if ($(this).scrollTop() >= $(item).offset().top - 400) {
             $(item).addClass("reveal");
+        }
+    });
+
+    $(window).scroll(function() {
+        if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+            var contactChild = document.querySelector('#contact > *');
+            contactChild.style.opacity = "1"
         }
     });
 });
