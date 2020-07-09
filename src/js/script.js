a413239c5977d0a798e2436d407ccbf757482210
coralcat <sori.coralcat@gmail.com>
2020-07-09 17:32:07 +0900

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,52 @@
+
+var cursor = $(".cursor"),
+    follower = $(".cursor-follower");
+
+var posX = 0,
+    posY = 0,
+    mouseX = 0,
+    mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+    repeat: -1,
+    onRepeat: function () {
+        posX += (mouseX - posX) / 9;
+        posY += (mouseY - posY) / 9;
+
+        TweenMax.set(follower, {
+            css: {
+                left: posX - 20,
+                top: posY - 20
+            }
+        });
+
+        TweenMax.set(cursor, {
+            css: {
+                left: mouseX,
+                top: mouseY
+            }
+        });
+    }
+});
+
+$(document).on("mousemove", function (e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+});
+
+$("#products img").on("mouseenter", function () {
+    cursor.addClass("active");
+    follower.addClass("active");
+});
+
+$("#products img").on("mouseleave", function () {
+    cursor.removeClass("active");
+    follower.removeClass("active");
+});
+
+
+
+
 $(function () {
 
     var fadeIn = $('section > *').toArray();
