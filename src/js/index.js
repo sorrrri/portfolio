dabd8b5be354267bba2316b74fcfe1404e8441dc
coralcat <sori.coralcat@gmail.com>
2020-09-07 17:01:33 +0900

    script 정리

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,35 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-
 window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 })
-
-
 document.documentElement.addEventListener('touchstart', function (event) {
-    if (event.touches.length > 1) {
-        event.preventDefault();
-    }
+  if (event.touches.length > 1) {
+    event.preventDefault();
+  }
 }, false);
 
+
+const filterCharts = document.querySelector('.filter-charts')
+if (filterCharts) {
+  // On checkbox click in the filter
+  $("#filters-container .checkbox").click(() => {
+    // Define constants for length of checked checkboxes array
+    const selectedChartLength = $('.filter-charts :checkbox').filter(':checked').length;
+    // Hide all items in the list
+    $(".chart-container").hide();
+
+    // If checkboxes are selected in the color-filter ONLY
+    if (selectedChartLength >= 1) {
+      // For each of the checked checkboxes in the color-filter
+      $(".filter-charts :checkbox:checked").each((index, element) => {
+        // Show items with the class of the value of the checkbox
+        $("." + $(element).val()).fadeIn();
+      });
+    }
+  })
+}
 
 
 // Modal
@@ -31,7 +48,7 @@ const ETCO2Modal = () => {
 }
 
 $(".close").on("click", function () {
-    $(".modal, .modal-backdrop").removeClass("active");
+  $(".modal, .modal-backdrop").removeClass("active");
 });
 
 const content = document.querySelector('.content')
@@ -42,106 +59,106 @@ const scrollToLeft = document.querySelector('.scroll-to-left')
 const scrollToRight = document.querySelector('.scroll-to-right')
 
 function toggleScrollHandler() {
-    scrollHandler.classList.toggle("active")
+  scrollHandler.classList.toggle("active")
 }
 
 function handleScrollToTop() {
-    content.scrollTo({top: 0, behavior: "smooth"})
+  content.scrollTo({top: 0, behavior: "smooth"})
 }
 
 function handleScrollToBottom() {
-    content.scrollTo({top: content.scrollHeight - content.clientHeight, behavior: "smooth"})
+  content.scrollTo({top: content.scrollHeight - content.clientHeight, behavior: "smooth"})
 }
 
 function handleScrollToLeft() {
-    content.scrollTo({left: 0, behavior: "smooth"})
+  content.scrollTo({left: 0, behavior: "smooth"})
 }
 
 function handleScrollToRight() {
-    content.scrollTo({left: content.scrollWidth - content.clientWidth, behavior: "smooth"})
+  content.scrollTo({left: content.scrollWidth - content.clientWidth, behavior: "smooth"})
 }
 
-if(scrollHandler) {
-    scrollHandler.addEventListener('click', toggleScrollHandler)
-    scrollToTop.addEventListener('click', handleScrollToTop)
-    scrollToBottom.addEventListener('click', handleScrollToBottom)
-    scrollToLeft.addEventListener('click', handleScrollToLeft)
-    scrollToRight.addEventListener('click', handleScrollToRight)
+if (scrollHandler) {
+  scrollHandler.addEventListener('click', toggleScrollHandler)
+  scrollToTop.addEventListener('click', handleScrollToTop)
+  scrollToBottom.addEventListener('click', handleScrollToBottom)
+  scrollToLeft.addEventListener('click', handleScrollToLeft)
+  scrollToRight.addEventListener('click', handleScrollToRight)
 
 }
-
 
 
 var dragSrcEl = null;
 
 function handleDragStart(e) {
-    // Target (this) element is the source node.
-    dragSrcEl = this;
+  // Target (this) element is the source node.
+  dragSrcEl = this;
 
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.outerHTML);
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.outerHTML);
 
-    this.classList.add('dragElem');
+  this.classList.add('dragElem');
 }
+
 function handleDragOver(e) {
-    if (e.preventDefault) {
-        e.preventDefault(); // Necessary. Allows us to drop.
-    }
-    this.classList.add('over');
+  if (e.preventDefault) {
+    e.preventDefault(); // Necessary. Allows us to drop.
+  }
+  this.classList.add('over');
 
-    e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
 
-    return false;
+  return false;
 }
 
 function handleDragEnter(e) {
-    // this / e.target is the current hover target.
+  // this / e.target is the current hover target.
 }
 
 function handleDragLeave(e) {
-    this.classList.remove('over');  // this / e.target is previous target element.
+  this.classList.remove('over');  // this / e.target is previous target element.
 }
 
 function handleDrop(e) {
-    // this/e.target is current target element.
+  // this/e.target is current target element.
 
-    if (e.stopPropagation) {
-        e.stopPropagation(); // Stops some browsers from redirecting.
-    }
+  if (e.stopPropagation) {
+    e.stopPropagation(); // Stops some browsers from redirecting.
+  }
 
-    // Don't do anything if dropping the same column we're dragging.
-    if (dragSrcEl != this) {
-        // Set the source column's HTML to the HTML of the column we dropped on.
-        //alert(this.outerHTML);
-        //dragSrcEl.innerHTML = this.innerHTML;
-        //this.innerHTML = e.dataTransfer.getData('text/html');
-        this.parentNode.removeChild(dragSrcEl);
-        var dropHTML = e.dataTransfer.getData('text/html');
-        this.insertAdjacentHTML('beforebegin',dropHTML);
-        var dropElem = this.previousSibling;
-        addDnDHandlers(dropElem);
+  // Don't do anything if dropping the same column we're dragging.
+  if (dragSrcEl != this) {
+    // Set the source column's HTML to the HTML of the column we dropped on.
+    //alert(this.outerHTML);
+    //dragSrcEl.innerHTML = this.innerHTML;
+    //this.innerHTML = e.dataTransfer.getData('text/html');
+    this.parentNode.removeChild(dragSrcEl);
+    var dropHTML = e.dataTransfer.getData('text/html');
+    this.insertAdjacentHTML('beforebegin', dropHTML);
+    var dropElem = this.previousSibling;
+    addDnDHandlers(dropElem);
 
-    }
-    this.classList.remove('over');
-    return false;
+  }
+  this.classList.remove('over');
+  return false;
 }
 
 function handleDragEnd(e) {
-    // this/e.target is the source node.
-    this.classList.remove('over');
+  // this/e.target is the source node.
+  this.classList.remove('over');
 
-    /*[].forEach.call(cols, function (col) {
-      col.classList.remove('over');
-    });*/
+  /*[].forEach.call(cols, function (col) {
+    col.classList.remove('over');
+  });*/
 }
 
 function addDnDHandlers(elem) {
-    elem.addEventListener('dragstart', handleDragStart, false);
-    elem.addEventListener('dragenter', handleDragEnter, false)
-    elem.addEventListener('dragover', handleDragOver, false);
-    elem.addEventListener('dragleave', handleDragLeave, false);
-    elem.addEventListener('drop', handleDrop, false);
-    elem.addEventListener('dragend', handleDragEnd, false);
+  elem.addEventListener('dragstart', handleDragStart, false);
+  elem.addEventListener('dragenter', handleDragEnter, false)
+  elem.addEventListener('dragover', handleDragOver, false);
+  elem.addEventListener('dragleave', handleDragLeave, false);
+  elem.addEventListener('drop', handleDrop, false);
+  elem.addEventListener('dragend', handleDragEnd, false);
 
 }
 
