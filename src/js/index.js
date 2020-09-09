a8c11ac662d9d13af4f0aaa93380ef39a38e2338
coralcat <sori.coralcat@gmail.com>
2020-09-09 14:45:02 +0900

    add countdown/refresh button animation delay 복구

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,33 @@ $(document).ready(function () {
 });
 
 
+function countdown(elementName, minutes, seconds) {
+  var element, endTime, hours, mins, msLeft, time;
+
+  function twoDigits(n) {
+    return (n <= 9 ? "0" + n : n);
+  }
+
+  function updateTimer() {
+    msLeft = endTime - (+new Date);
+
+    time = new Date(msLeft);
+    hours = time.getUTCHours();
+    mins = time.getUTCMinutes();
+    element.innerHTML = (hours ? hours + ':' + twoDigits(mins) : mins) + ':' + twoDigits(time.getUTCSeconds());
+    setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
+
+  }
+
+  element = document.getElementById(elementName);
+  endTime = (+new Date) + 1000 * (60 * minutes + seconds) + 500;
+  updateTimer();
+}
+
+countdown("countdown", 10, 0);
+
+
+
 const filterCharts = document.querySelector('.filter-charts')
 if (filterCharts) {
   // On checkbox click in the filter
