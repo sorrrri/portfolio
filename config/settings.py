ba1c87c1f8d665722679e13c3f963abd05a8baec
soridive <soridive@naver.com>
2020-10-06 10:57:50 +0900

    도메인 연결 debug false

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ ROOT_DIR = os.path.dirname(BASE_DIR)
 SECRET_KEY = 'y83s5hw&$b5)7(r3$e&eq%8h&rba$bh-#%23)q(2tsyvh8xomh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".ap-northeast-2.compute.amazonaws.com",
