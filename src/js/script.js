dfd9c8d2f657ad885a9a3076d1a62952b5e75fec
coralcat <sori.coralcat@gmail.com>
2020-10-07 17:34:31 +0900

    대시보드 작업중

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
+document.documentElement.addEventListener('touchstart', function (event) {
+  if (event.touches.length > 1) {
+    event.preventDefault()
+  }
+}, false)
+
+document.addEventListener('DOMContentLoaded', function () {
+  const subMenu = document.querySelector('.sub-menu')
+
+  if (document.location.hash == "" || document.location.hash == "#")
+    document.location.hash = "#all";
+
+  function activeClass(e) {
+    var elems = document.querySelectorAll(".active");
+
+    Array.from(elems).forEach(el => {
+      el.classList.remove('active');
+    })
+    // mark as active selected menu item
+    e.target.classList.add("active");
+  }
+
+  if (subMenu) {
+    subMenu.addEventListener('click', activeClass)
+    subMenu.addEventListener('mouseover', () => {
+      document.getElementsByClassName('container').style.backgroundPosition = "100% 0"
+    })
+  }
+})
