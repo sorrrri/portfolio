bc7eafb100b51cd4aa673490edf6d5e96db66d8a
sori <sori.coralcat@gmail.com>
2021-05-18 15:46:33 +0900

    메인페이지 작업

---
+++ src/assets/js/script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Tab Menu
+    const tabMenus = document.querySelectorAll('.tab-menu li')
+    const linkButton = document.querySelector('.tab-menu button')
+    const tabContents = document.querySelectorAll('.tab-content section')
+
+
+    const activeSection = (e) => {
+        e.stopPropagation();
+        let menuIndex = [...tabMenus].indexOf(e.target);
+
+        tabMenus.forEach(menu => {
+            [...tabMenus].indexOf(menu) === menuIndex
+                ? menu.classList.add('active')
+                : menu.classList.remove('active')
+        });
+
+        tabContents.forEach(content => {
+            [...tabContents].indexOf(content) === menuIndex
+                ? content.classList.add('active')
+                : content.classList.remove('active')
+        });
+    };
+
+    [...tabMenus][0].classList.add('active');
+    [...tabContents][0].classList.add('active');
+
+    tabMenus.forEach(menu => {
+        menu.addEventListener('click', activeSection);
+    })
+
+
+    const notice = document.querySelector('.notice')
+    const downloads = document.querySelector('.downloads')
+    const errata = document.querySelector('.errata')
+
+    linkButton.addEventListener("click", (e) => {
+      e.stopPropagation();
+      if(notice.classList.contains("active")) {
+        window.location = "http://www.naver.com";
+      } else if (downloads.classList.contains("active")) {
+        window.location = "http://www.daum.net";
+      } else if (errata.classList.contains("active")) {
+        window.location = "http://www.google.com";
+      }
+    })
+})
