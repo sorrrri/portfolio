e2dead4d9a54ef9c47ae9d94ee9cf6c8aa43f0d6
sori <sori.coralcat@gmail.com>
2021-05-25 15:46:57 +0900

    Admin 1차 완료

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -47,21 +47,190 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  const admin = document.querySelector(".admin")
+  const admin = document.querySelector(".admin");
 
-  if(admin) {
-    
-    const mainMenus = document.querySelectorAll('.main-menu')
+  if (admin) {
+    const mainMenus = document.querySelectorAll(".main-menu");
 
     mainMenus.forEach((mainMenu) => {
-        const subMenu = mainMenu.nextElementSibling
+      const subMenu = mainMenu.nextElementSibling;
 
-        if(subMenu) {
-            mainMenu.addEventListener('click', () => {
-                mainMenu.classList.toggle('active')
-                subMenu.classList.toggle('active')
-            })
-        }
-    })
+      if (subMenu) {
+        mainMenu.addEventListener("click", () => {
+          mainMenu.classList.toggle("active");
+          subMenu.classList.toggle("active");
+        });
+      }
+    });
   }
+
+  const fileInput = document.querySelector(".input-file"),
+    fileButton = document.querySelector(".input-file-trigger"),
+    the_return = document.querySelector(".file-return");
+
+  if (fileInput) {
+    fileButton.addEventListener("keydown", function (event) {
+      if (event.keyCode == 13 || event.keyCode == 32) {
+        fileInput.focus();
+      }
+    });
+    fileButton.addEventListener("click", function (event) {
+      fileInput.focus();
+      return false;
+    });
+    fileInput.addEventListener("change", function (event) {
+      the_return.innerHTML = this.files[0].name;
+    });
+  }
+
+
+  // Checkbox All
+  function check() {
+    var checked =
+      arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
+    var cbs = document.querySelectorAll('input[name="books"]');
+    cbs.forEach(function (cb) {
+      cb.checked = checked;
+    });
+  }
+
+  function handleCheckAll() {
+    check();
+    this.onclick = handleUncheckAll;
+  }
+
+  function handleUncheckAll() {
+    check(false);
+    this.onclick = handleCheckAll;
+  }
+
+  const btnCheckAll = document.querySelector("#checkAll");
+
+  if (btnCheckAll) {
+    btnCheckAll.addEventListener("click", handleCheckAll);
+  }
+
+
+
+
+  // Admin: Logo File Upload
+  function ekUpload() {
+    function Init() {
+      console.log("Upload Initialised");
+
+      var fileSelect = document.getElementById("file-upload"),
+        fileDrag = document.getElementById("file-drag"),
+        submitButton = document.getElementById("submit-button");
+
+      fileSelect.addEventListener("change", fileSelectHandler, false);
+
+      // Is XHR2 available?
+      var xhr = new XMLHttpRequest();
+      if (xhr.upload) {
+        // File Drop
+        fileDrag.addEventListener("dragover", fileDragHover, false);
+        fileDrag.addEventListener("dragleave", fileDragHover, false);
+        fileDrag.addEventListener("drop", fileSelectHandler, false);
+      }
+    }
+
+    function fileDragHover(e) {
+      var fileDrag = document.getElementById("file-drag");
+
+      e.stopPropagation();
+      e.preventDefault();
+
+      fileDrag.className =
+        e.type === "dragover" ? "hover" : "modal-body file-upload";
+    }
+
+    function fileSelectHandler(e) {
+      // Fetch FileList object
+      var files = e.target.files || e.dataTransfer.files;
+
+      // Cancel event and hover styling
+      fileDragHover(e);
+
+      // Process all File objects
+      for (var i = 0, f; (f = files[i]); i++) {
+        parseFile(f);
+        uploadFile(f);
+      }
+    }
+
+    // Output
+    function output(msg) {
+      // Response
+      var m = document.getElementById("messages");
+      m.innerHTML = msg;
+    }
+
+    function parseFile(file) {
+      console.log(file.name);
+      output("<strong>" + encodeURI(file.name) + "</strong>");
+
+      // var fileType = file.type;
+      // console.log(fileType);
+      var imageName = file.name;
+
+      var isGood = /\.(?=gif|jpg|png|jpeg)/gi.test(imageName);
+      if (isGood) {
+        document.getElementById("start").classList.add("hidden");
+        document.getElementById("response").classList.remove("hidden");
+        document.getElementById("notimage").classList.add("hidden");
+        // Thumbnail Preview
+        document.getElementById("file-image").classList.remove("hidden");
+        document.getElementById("file-image").src = URL.createObjectURL(file);
+      } else {
+        document.getElementById("file-image").classList.add("hidden");
+        document.getElementById("notimage").classList.remove("hidden");
+        document.getElementById("start").classList.remove("hidden");
+        document.getElementById("response").classList.add("hidden");
+        document.getElementById("file-upload-form").reset();
+      }
+    }
+    function uploadFile(file) {
+      var xhr = new XMLHttpRequest(),
+        fileInput = document.getElementById("class-roster-file"),
+        pBar = document.getElementById("file-progress"),
+        fileSizeLimit = 1024; // In MB
+      if (xhr.upload) {
+        // Check if file is less than x MB
+        if (file.size <= fileSizeLimit * 1024 * 1024) {
+          // Progress bar
+          pBar.style.display = "inline";
+
+          // File received / failed
+          xhr.onreadystatechange = function (e) {
+            if (xhr.readyState == 4) {
+              // Everything is good!
+              // progress.className = (xhr.status == 200 ? "success" : "failure");
+              // document.location.reload(true);
+            }
+          };
+
+          // Start upload
+          xhr.open(
+            "POST",
+            document.getElementById("file-upload-form").action,
+            true
+          );
+          xhr.setRequestHeader("X-File-Name", file.name);
+          xhr.setRequestHeader("X-File-Size", file.size);
+          xhr.setRequestHeader("Content-Type", "multipart/form-data");
+          xhr.send(file);
+        } else {
+          output("Please upload a smaller file (< " + fileSizeLimit + " MB).");
+        }
+      }
+    }
+
+    // Check for the various File API support.
+    if (window.File && window.FileList && window.FileReader) {
+      Init();
+    } else {
+      document.getElementById("file-drag").style.display = "none";
+    }
+  }
+  ekUpload();
 });
