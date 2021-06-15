e6204ede9bd1cacce51d580859dff321cfb269d4
sori <sori.coralcat@gmail.com>
2021-06-15 18:02:25 +0900

    스타일, 자바스크립트 수정

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   });
 
+  const header = document.querySelector("header");
   const mainMenus = document.querySelectorAll(".main-menu");
   const globalNavigationMenu = document.querySelector(
     ".global-navigation-menu"
@@ -34,40 +35,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const inputSearchFull = document.querySelector(".input-search-full");
   const modalSearchFull = document.querySelector(".modal-search-full");
-  
+
   if (inputSearchFull) {
-    
     const handleActiveSearchFull = () => {
       if (modalSearchFull.classList.contains("active") === false) {
         let button = document.createElement("button");
         modalSearchFull.classList.add("active");
-        const header = document.querySelector("header");
         header.replaceChild(button, toggleMenu);
+
         const buttonBack = button;
         buttonBack.classList.add("btn-back");
-
         buttonBack.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
         <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z" />
       </svg>`;
 
         buttonBack.addEventListener("click", () => {
-          button = document.createElement("button");
           modalSearchFull.classList.remove("active");
-          header.replaceChild(button, buttonBack);
-          toggleMenu = button;
-          toggleMenu.classList.add("toggle-menu");
-
-          toggleMenu.innerHTML = `<svg viewBox="0 0 100 100" width="80">
-              <path
-                d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
-              <path d="m 30,50 h 40" />
-              <path
-                d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
-            </svg>`;
-          button.addEventListener("click", () => {
-            globalNavigationMenu.classList.add("active");
-            overlay.classList.add("active");
-          });
+          header.replaceChild(toggleMenu, buttonBack);
         });
       }
     };
@@ -75,31 +59,39 @@ document.addEventListener("DOMContentLoaded", () => {
     inputSearchFull.addEventListener("click", handleActiveSearchFull);
   }
 
+
+  // Toggle Menu
   if (toggleMenu) {
-    toggleMenu.addEventListener("click", (e) => {
-      e.stopPropagation();
-      globalNavigationMenu.classList.add("active");
+    const openNavigationMenu = (menu) => {
+      menu.classList.add("active");
       overlay.classList.add("active");
+    };
+
+    const closeNavigationMenu = (toggleButton, menu) => {
+      toggleButton.classList.remove("active");
+      menu.classList.remove("active");
+    };
+
+    toggleMenu.addEventListener("click", () => {
+      openNavigationMenu(globalNavigationMenu);
     });
 
+    if (localToggleMenu) {
+      localToggleMenu.addEventListener("click", () => {
+        openNavigationMenu(localNavigationMenu);
+      });
+    }
+
     overlay.addEventListener("click", () => {
-      toggleMenu.classList.remove("active");
-      globalNavigationMenu.classList.remove("active");
+      closeNavigationMenu(toggleMenu, globalNavigationMenu);
+
+      if (localToggleMenu) {
+        closeNavigationMenu(localToggleMenu, localNavigationMenu);
+      }
     });
   }
 
-  if (localToggleMenu) {
-    localToggleMenu.addEventListener("click", (e) => {
-      e.stopPropagation();
-      localNavigationMenu.classList.add("active");
-      overlay.classList.add("active");
-    });
 
-    overlay.addEventListener("click", () => {
-      localToggleMenu.classList.remove("active");
-      localNavigationMenu.classList.remove("active");
-    });
-  }
 
   // Tab Menu
   const main = document.querySelector(".main");
@@ -205,8 +197,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (moveDown) {
     moveDown.addEventListener("click", () => {
-      markerDetails.classList.remove("active")
-    })
+      markerDetails.classList.remove("active");
+    });
     // let clientY, deltaY;
 
     // moveDown.addEventListener(
