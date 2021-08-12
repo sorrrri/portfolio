10f16ff914fe4a802e3c98919736b88a855a8130
Sori Kim <sori.coralcat@gmail.com>
2021-08-12 16:11:03 +0900

    메인 탭메뉴 스크립트 제거, li에서 div로 태그변경

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -10,50 +10,50 @@ document.addEventListener("DOMContentLoaded", () => {
   // Tab Menu
   const main = document.querySelector(".main");
 
-  if (main) {
-    const tabMenus = document.querySelectorAll(".tab-menu li");
-    const linkButton = document.querySelector(".tab-menu button");
-    const tabContents = document.querySelectorAll(".tab-content section");
+  // if (main) {
+  //   const tabMenus = document.querySelectorAll(".tab-menu li");
+  //   const linkButton = document.querySelector(".tab-menu button");
+  //   const tabContents = document.querySelectorAll(".tab-content section");
 
-    const activeSection = (e) => {
-      e.stopPropagation();
-      let menuIndex = [...tabMenus].indexOf(e.target);
+  //   const activeSection = (e) => {
+  //     e.stopPropagation();
+  //     let menuIndex = [...tabMenus].indexOf(e.target);
 
-      tabMenus.forEach((menu) => {
-        [...tabMenus].indexOf(menu) === menuIndex
-          ? menu.classList.add("active")
-          : menu.classList.remove("active");
-      });
+  //     tabMenus.forEach((menu) => {
+  //       [...tabMenus].indexOf(menu) === menuIndex
+  //         ? menu.classList.add("active")
+  //         : menu.classList.remove("active");
+  //     });
 
-      tabContents.forEach((content) => {
-        [...tabContents].indexOf(content) === menuIndex
-          ? content.classList.add("active")
-          : content.classList.remove("active");
-      });
-    };
+  //     tabContents.forEach((content) => {
+  //       [...tabContents].indexOf(content) === menuIndex
+  //         ? content.classList.add("active")
+  //         : content.classList.remove("active");
+  //     });
+  //   };
 
-    [...tabMenus][0].classList.add("active");
-    [...tabContents][0].classList.add("active");
+  //   [...tabMenus][0].classList.add("active");
+  //   [...tabContents][0].classList.add("active");
 
-    tabMenus.forEach((menu) => {
-      menu.addEventListener("click", activeSection);
-    });
+  //   tabMenus.forEach((menu) => {
+  //     menu.addEventListener("click", activeSection);
+  //   });
 
-    const notice = document.querySelector(".notice");
-    const downloads = document.querySelector(".downloads");
-    const errata = document.querySelector(".errata");
+  //   const notice = document.querySelector(".notice");
+  //   const downloads = document.querySelector(".downloads");
+  //   const errata = document.querySelector(".errata");
 
-    linkButton.addEventListener("click", (e) => {
-      e.stopPropagation();
-      if (notice.classList.contains("active")) {
-        window.location = "notice.html";
-      } else if (downloads.classList.contains("active")) {
-        window.location = "downloads.html";
-      } else if (errata.classList.contains("active")) {
-        window.location = "errata.html";
-      }
-    });
-  }
+  //   linkButton.addEventListener("click", (e) => {
+  //     e.stopPropagation();
+  //     if (notice.classList.contains("active")) {
+  //       window.location = "notice.html";
+  //     } else if (downloads.classList.contains("active")) {
+  //       window.location = "downloads.html";
+  //     } else if (errata.classList.contains("active")) {
+  //       window.location = "errata.html";
+  //     }
+  //   });
+  // }
 
   const admin = document.querySelector(".admin");
 
