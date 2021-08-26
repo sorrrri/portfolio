8ba3ebdf6ab7761c9b0f52a611b009960dda0b43
박지훈 <dingcoding@jm-soft.co.kr>
2021-08-26 10:08:50 +0900

    Keycloak 테스트 중

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,40 @@ import {
   Switch,
 } from 'react-router-dom';
 import './App.css';
+import { SSRKeycloakProvider, SSRCookies } from '@react-keycloak/ssr';
+import { Cookies } from 'react-cookie';
 import { LayoutTopNavigator } from './_layout';
 import { Workspace } from './pages/workspace';
 import { Device } from './pages/device';
 
 function App(routeProps: RouteComponentProps) {
+  const keycloackConfig = {
+    url: 'https://auth-172-30-10-101.vurix.kr/auth/',
+    realm: 'platform',
+    clientId: 'platform',
+  };
+
+  const cookies = new Cookies();
+
   return (
-    <Route path="*">
-      <LayoutTopNavigator {...routeProps}>
-        <Switch>
-          <Route path="/workspace" component={Workspace} />
-          <Route path="/device" component={Device} />
-          <Redirect push to="/workspace" />
-        </Switch>
-      </LayoutTopNavigator>
-    </Route>
+    <SSRKeycloakProvider
+      initOptions={{
+        onLoad: 'login-required',
+      }}
+      onEvent={(type, error) => console.log(error)}
+      keycloakConfig={keycloackConfig}
+      persistor={SSRCookies(cookies)}
+    >
+      <Route path="*">
+        <LayoutTopNavigator {...routeProps}>
+          <Switch>
+            <Route path="/workspace" component={Workspace} />
+            <Route path="/device" component={Device} />
+            <Redirect push to="/workspace" />
+          </Switch>
+        </LayoutTopNavigator>
+      </Route>
+    </SSRKeycloakProvider>
   );
 }
 
