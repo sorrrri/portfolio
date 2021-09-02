e1bc85f824c205f942aaf127f7b8aca2005c7f7b
Sori Kim <sori.coralcat@gmail.com>
2021-09-02 12:02:25 +0900

    bottom sticky menu: dim 처리

--- src/_layout/bottom-sticky-menu.tsx
+++ src/_layout/bottom-sticky-menu.tsx
@@ -11,25 +11,32 @@ export const BottomStickyMenu = (props: { toggle: any }) => {
   };
 
   return (
-    <div className="bottom-sticky-menu">
-      <div className={`buttons ${isToggleOn ? 'active' : ''}`}>
-        <button className="btn-search" onClick={toggle} type="button">
-          <i className="fad fa-search" />
-        </button>
-        <button
-          onClick={() => history.push('/workspace-add')}
-          className="btn-workspace"
-          type="button"
-        >
-          <i className="fad fa-briefcase" />
-        </button>
-        <button onClick={() => history.push('/device-add')} className="btn-undertake" type="button">
-          <i className="fad fa-tools" />
+    <>
+      <div className={`overlay ${isToggleOn ? 'active' : ''}`} onClick={() => setToggleOn(false)} />
+      <div className="bottom-sticky-menu">
+        <div className={`buttons ${isToggleOn ? 'active' : ''}`}>
+          <button className="btn-search" onClick={toggle} type="button">
+            <i className="fad fa-search" />
+          </button>
+          <button
+            onClick={() => history.push('/workspace-add')}
+            className="btn-workspace"
+            type="button"
+          >
+            <i className="fad fa-briefcase" />
+          </button>
+          <button
+            onClick={() => history.push('/device-add')}
+            className="btn-undertake"
+            type="button"
+          >
+            <i className="fad fa-tools" />
+          </button>
+        </div>
+        <button className="btn-main" onClick={toggleMenus} type="button">
+          <i className="fal fa-plus" />
         </button>
       </div>
-      <button className="btn-main" onClick={toggleMenus} type="button">
-        <i className="fal fa-plus" />
-      </button>
-    </div>
+    </>
   );
 };
