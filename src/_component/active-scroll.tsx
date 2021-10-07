d818fa1cfc3b017c0b1b810aa410d7f0bf5383b7
Sori Kim <sori.coralcat@gmail.com>
2021-10-07 14:45:10 +0900

    infinite scroll 기능 구현듕

--- src/_component/active-scroll.tsx
+++ src/_component/active-scroll.tsx
@@ -3,21 +3,13 @@
 export const ActiveScroll = () => {
   const container = document.querySelector('.container') as HTMLDivElement;
   const main = document.querySelector('main') as HTMLDivElement;
-  const workspaceList = document.querySelector('main.workspace.list');
   const mapPage = document.querySelector('.modal-marker') as HTMLDivElement;
-  // const bottomStickyMenu = document.querySelector('.bottom-sticky-menu') as HTMLDivElement;
 
   if (main && !mapPage) {
     if (main.scrollTop > 50) {
       container.classList.add('scroll');
     } else {
       container.classList.remove('scroll');
-    }
-
-    if (workspaceList) {
-      if (main.scrollHeight - (main.scrollTop + main.offsetHeight) < 50) {
-        console.log('test');
-      }
     }
   }
 };
--- src/pages/workspace/workspace-list.tsx
+++ src/pages/workspace/workspace-list.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { showHeader } from '../../_store/slice/header-option';
 import { AddSearchWork } from '../../_layout/top-navigator/right-context/add-search-work';
@@ -64,28 +67,76 @@ export function WorkspaceList(props: any) {
     history.push(`/workspace/${workId}`);
   };
 
+  const ListItem = () => (
+    <>
+      {searchWorkspaceList.map((workdata) => (
+        <Row
+          key={workdata.work_uuid}
+          item={() => onClickItem(workdata.work_uuid)}
+          rowtype={workdata.type}
+          title={workdata.title}
+          writer={workdata.registrant.name}
+          date={workdata.reg_date}
+          worktype={workdata.type}
+          importance={workdata.priority}
+          attachments={workdata.attachments}
+          comment={workdata.comment}
+          read={workdata.views}
+          images={workdata.attachments_preview}
+        >
+          {workdata.summary_content}
+        </Row>
+      ))}
+    </>
+  );
+
+  const fakeFetch = (delay = 1000) => new Promise((res) => setTimeout(res, delay));
+  // const fetchItems = async () => searchWorkspaceList.map((workdata) => workdata + 3);
+  const [state, setState] = useState({ itemCount: 0, isLoading: false });
+  const fetchItems = async () => {
+    setState((prev) => ({ ...prev, isLoading: true }));
+    await fakeFetch();
+    setState((prev) => ({
+      itemCount: prev.itemCount + 5,
+      isLoading: false,
+    }));
+  };
+  const [target, setTarget] = useState<HTMLDivElement | null>(null);
+
+  const checkIntersect = async ([entry]: any, observer: any) => {
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target);
+      await fetchItems();
+      observer.observe(entry.target);
+    }
+  };
+  const options = {
+    root: document.querySelector('main'),
+    rootMargin: '0px',
+    threshold: 1.0,
+  };
+
+  useEffect(() => {
+    let observer: any;
+    if (target) {
+      observer = new IntersectionObserver(checkIntersect, options);
+      observer.observe(target);
+    }
+    return () => observer && observer.disconnect();
+  }, [target]);
+
+  const { itemCount, isLoading } = state;
+
   return (
     <>
       <SearchArea show={isToggleOn} onChange={(keyword) => setSearch(keyword)} />
       <main className="content list workspace" onScroll={ActiveScroll}>
-        {searchWorkspaceList.map((workdata) => (
-          <Row
-            key={workdata.work_uuid}
-            item={() => onClickItem(workdata.work_uuid)}
-            rowtype={workdata.type}
-            title={workdata.title}
-            writer={workdata.registrant.name}
-            date={workdata.reg_date}
-            worktype={workdata.type}
-            importance={workdata.priority}
-            attachments={workdata.attachments}
-            comment={workdata.comment}
-            read={workdata.views}
-            images={workdata.attachments_preview}
-          >
-            {workdata.summary_content}
-          </Row>
-        ))}
+        {[...Array(itemCount)].map((_, index) => {
+          return <ListItem key={index} />;
+        })}
+        <div ref={setTarget} className="Loading">
+          {isLoading && 'Loading...'}
+        </div>
       </main>
       <main className="no-result">
         <div>
