051d5bcb147e30ea74d34a69247abf576e3e9e2a
doori <doori.lee@jm-soft.co.kr>
2021-10-08 09:15:17 +0900

    충돌/오류 수정

--- src/pages/workspace/workspace-list.tsx
+++ src/pages/workspace/workspace-list.tsx
@@ -88,66 +88,6 @@ export function WorkspaceList(props: any) {
     history.push(`/workspace/${workId}`);
   };
 
-  const ListItem = () => (
-    <>
-      {searchWorkspaceList.map((workdata) => (
-        <Row
-          key={workdata.work_uuid}
-          item={() => onClickItem(workdata.work_uuid)}
-          rowtype={workdata.type}
-          title={workdata.title}
-          writer={workdata.registrant.name}
-          date={workdata.reg_date}
-          worktype={workdata.type}
-          importance={workdata.priority}
-          attachments={workdata.attachments}
-          comment={workdata.comment}
-          read={workdata.views}
-          images={workdata.attachments_preview}
-        >
-          {workdata.summary_content}
-        </Row>
-      ))}
-    </>
-  );
-
-  const fakeFetch = (delay = 1000) => new Promise((res) => setTimeout(res, delay));
-  // const fetchItems = async () => searchWorkspaceList.map((workdata) => workdata + 3);
-  const [state, setState] = useState({ itemCount: 0, isLoading: false });
-  const fetchItems = async () => {
-    setState((prev) => ({ ...prev, isLoading: true }));
-    await fakeFetch();
-    setState((prev) => ({
-      itemCount: prev.itemCount + 5,
-      isLoading: false,
-    }));
-  };
-  const [target, setTarget] = useState<HTMLDivElement | null>(null);
-
-  const checkIntersect = async ([entry]: any, observer: any) => {
-    if (entry.isIntersecting) {
-      observer.unobserve(entry.target);
-      await fetchItems();
-      observer.observe(entry.target);
-    }
-  };
-  const options = {
-    root: document.querySelector('main'),
-    rootMargin: '0px',
-    threshold: 1.0,
-  };
-
-  useEffect(() => {
-    let observer: any;
-    if (target) {
-      observer = new IntersectionObserver(checkIntersect, options);
-      observer.observe(target);
-    }
-    return () => observer && observer.disconnect();
-  }, [target]);
-
-  const { itemCount, isLoading } = state;
-
   return (
     <>
       <SearchArea show={isToggleOn} onChange={(keyword) => setSearch(keyword)} />
