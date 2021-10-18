860e700b5649d800f94692061d42332a6fadac38
dingcoding <dingcoding@jm-soft.co.kr>
2021-10-18 16:12:21 +0900

    devextreme 아이콘 버그 수정

--- src/_component/html-editor.tsx
+++ src/_component/html-editor.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import HtmlEditor, { Toolbar, MediaResizing, Item } from 'devextreme-react/html-editor';
 import CheckBox from 'devextreme-react/check-box';
 import DateBox from 'devextreme-react/date-box';
-import './html-editor.css';
 
 export const TextEditor = (props: any) => {
   const sizeValues = ['8pt', '10pt', '12pt', '14pt', '18pt', '24pt', '36pt'];
